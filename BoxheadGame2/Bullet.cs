6f7a72c83bc5ecd2b6b439f92c0d6ab267b2a791
Han Thi Nguyen <sunakarus@gmail.com>
2014-03-03 23:18:13 +0100

    testing

--- BoxheadGame2/Bullet.cs
+++ BoxheadGame2/Bullet.cs
@@ -1,14 +1,10 @@
 ﻿using CircleCollisions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace BoxheadGame2
 {
-    class Bullet
+    internal class Bullet
     {
         public Bullet(Texture2D texture, Vector2 position, Vector2 direction)
         {
@@ -16,30 +12,36 @@ namespace BoxheadGame2
             this.position = position;
             this.direction = direction;
             origin = new Vector2(texture.Width / 2, texture.Height / 2);
-            hitbox = new Circle(texture.Width / 2, position);
+
+            scale = radius * 2 / texture.Width;
+            hitbox = new Circle(radius, position);
         }
 
         public Texture2D texture { get; set; }
+
         public Vector2 position { get; set; }
+
         public Vector2 direction { get; set; }
-        Vector2 origin;
-        float speed = 10f;
+
+        private Vector2 origin;
+        private float speed = 15f;
         public float lifeTime = 150f; //5 sec
         public Circle hitbox;
+
+        public float scale;
+        public static float radius = 4f;
 
         public void Update()
         {
             direction.Normalize();
             position += direction * speed;
-            if (lifeTime>0) { lifeTime--; }
+            if (lifeTime > 0) { lifeTime--; }
             hitbox.position = position;
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(texture, position, null, Color.White, 0, origin, 1, SpriteEffects.None, 1);
+            spriteBatch.Draw(texture, position, null, Color.White, 0, origin, scale, SpriteEffects.None, 1);
         }
-
-        
     }
 }
--- BoxheadGame2/Controller.cs
+++ BoxheadGame2/Controller.cs
@@ -1,22 +1,23 @@
 ﻿using CircleCollisions;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace BoxheadGame2
 {
-    class Controller
+    internal class Controller
     {
         public List<Bullet> bulletList = new List<Bullet>();
         public List<Crate> crateList = new List<Crate>();
         public List<Enemy> enemyList = new List<Enemy>();
 
         public Player player;
-        
+
+        public Texture2D tZombieAttack;
+        public Texture2D tPlayer, tCrate, tBullet, tZombie;
+        public SpriteFont font;
+
         public Controller(Player player)
         {
             this.player = player;
@@ -24,23 +25,18 @@ namespace BoxheadGame2
 
         public void Update()
         {
-            
-
-            for (int i = bulletList.Count -1; i > -1; i--)
+            for (int i = bulletList.Count - 1; i > -1; i--)
             {
                 bool breakAll = false;
 
                 Bullet b = bulletList[i];
                 b.Update();
 
-
                 foreach (Crate c in crateList)
                 {
                     if (b.hitbox.CollidesWithRect(c.hitbox))
                     {
-
                         bulletList.RemoveAt(i);
-                        Console.WriteLine(" - " + bulletList.Count);
                         breakAll = true;
                         break;
                     }
@@ -49,18 +45,14 @@ namespace BoxheadGame2
                 if (breakAll)
                 { break; }
 
-                if (enemyList.Count > 0)
+                foreach (Enemy e in enemyList)
                 {
-                    foreach (Enemy e in enemyList)
+                    if (b.hitbox.CollidesWithCircle(e.hitbox))
                     {
-                        if (b.hitbox.CollidesWithCircle(e.hitbox))
-                        {
-                            bulletList.RemoveAt(i);
-                            Console.WriteLine(" - " + bulletList.Count);
-                            e.state = Enemy.State.Dead;
-                            breakAll = true;
-                            break;
-                        }
+                        bulletList.RemoveAt(i);
+                        e.Death();
+                        breakAll = true;
+                        break;
                     }
                 }
 
@@ -70,14 +62,23 @@ namespace BoxheadGame2
                 if (b.lifeTime <= 0)
                 {
                     bulletList.RemoveAt(i);
-                    Console.WriteLine(" - " + bulletList.Count);
                     break;
                 }
             }
 
-            for (int i = enemyList.Count - 1; i>-1; i--)
+            for (int i = enemyList.Count - 1; i > -1; i--)
             {
                 enemyList[i].Update();
+
+                if (enemyList[i].state == Enemy.State.Attack && !player.invincible && enemyList[i].attackDelay.value <= 0)
+                {
+                    player.health -= enemyList[i].damage;
+                    Console.WriteLine(player.health);
+                    enemyList[i].attackDelay.Reset();
+                    player.invincible = true;
+                    player.invincibleTimer.Reset();
+                }
+
                 if (enemyList[i].state == Enemy.State.Dead)
                 {
                     enemyList.RemoveAt(i);
@@ -121,7 +122,6 @@ namespace BoxheadGame2
 
         public void GenerateEnemyZombie(Texture2D texture, Rectangle area)
         {
-
             Vector2 pos;
             EnemyZombie a;
             int counter = 10;
@@ -129,11 +129,11 @@ namespace BoxheadGame2
             do
             {
                 pos = new Vector2(Game1.random.Next(area.Width + 1) + area.X, Game1.random.Next(area.Height + 1) + area.Y);
-                a = new EnemyZombie(texture, pos, player, this);
+                a = new EnemyZombie(texture, pos, player, this, tZombieAttack);
                 counter--;
             }
 
-            while ((CollidesWithWall(a.hitbox) || CollidesWithPlayer(a.hitbox) || a.GetEnemyCollision(a.hitbox, Vector2.Zero)) && (counter>0));
+            while ((CollidesWithWall(a.hitbox) || CollidesWithPlayer(a.hitbox) || a.GetEnemyCollision(a.hitbox, Vector2.Zero)) && (counter > 0));
             if (counter > 0)
             {
                 this.enemyList.Add(a);
@@ -146,23 +146,22 @@ namespace BoxheadGame2
 
         public void Draw(SpriteBatch spriteBatch)
         {
-
             foreach (Crate c in crateList)
             {
                 c.Draw(spriteBatch);
             }
-            foreach(Bullet b in bulletList)
+            foreach (Bullet b in bulletList)
             {
                 b.Draw(spriteBatch);
             }
-            foreach(Enemy e in enemyList)
+            foreach (Enemy e in enemyList)
             {
                 if (e is EnemyZombie)
                 {
-                   (e as EnemyZombie).Draw(spriteBatch);
+                    (e as EnemyZombie).Draw(spriteBatch);
                 }
             }
-
+            spriteBatch.DrawString(font, "HEALTH: " + player.health, Vector2.Zero, Color.White);
         }
     }
 }
